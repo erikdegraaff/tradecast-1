cff61dff937ec5287faa3f1b4c2d5b47246e0af2
Erik de Graaff <38424388+erikdegraaff@users.noreply.github.com>
2023-12-08 00:07:22 +0100

    Added data JSON

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
-// import Button from './components/Button/Button'
+//'use client'
+
+//import { useState, useEffect } from 'react'
 import Marquee from './components/Marquee/Marquee'
 import Video from './components/Video/Video'
 import Notifications from './components/Notifications/Notifications'
 import styles from './page.module.scss'
 
+import data from "./data/data.json"
+
+
 export default function Home() {
 
-  const marqueeTexts = ['Halloa', 'world', 'how', 'are', 'you', 'today']
+  const marqueeTexts:string[] = []
+  
+  data.ticker.map((tick) => {
+    marqueeTexts.push(tick.body)
+  })
   
   return (
     <div className={styles.page}>
       <Notifications />
       <Video />
-      <Marquee texts={marqueeTexts} delimiter='&nbsp;&bull;&nbsp;' duration={0.8} />
+      <Marquee texts={marqueeTexts} delimiter='&nbsp;&bull;&nbsp;' duration={0.5} />
     </div>
   )
 }
